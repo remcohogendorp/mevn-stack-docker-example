35b96c3f2ef4ebc7255bf81b81090dae521e3157
Remco Hogendorp <historemc.developer@gmail.com>
2021-10-17 22:54:28 +0200

    Initial commit

---
+++ frontend/src/views/Home.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="home">
+    <img alt="Vue logo" src="../assets/logo.png" />
+    <HelloWorld :msg="msg" />
+  </div>
+</template>
+
+<script lang="ts">
+import { Options, Vue } from "vue-class-component";
+import HelloWorld from "@/components/HelloWorld.vue"; // @ is an alias to /src
+import axios from "axios";
+
+@Options({
+  components: {
+    HelloWorld,
+  },
+})
+export default class Home extends Vue {
+
+  msg = "";
+
+  mounted(): void {
+    axios.get(process.env.VUE_APP_BACKEND_BASE_URL).then((res) => {
+      this.msg = res.data as string;
+    });
+  }
+}
+</script>
