404a0f64a24d319623f69de45d55021b93ade96f
Remco Hogendorp <historemc.developer@gmail.com>
2021-10-21 22:54:46 +0200

    Moved env variable to dedicated folder

---
+++ frontend/src/config/config.ts
@@ -0,0 +1,3 @@
+const BACKEND_BASE_URL = process.env.VUE_APP_BACKEND_BASE_URL as string;
+
+export { BACKEND_BASE_URL };
--- frontend/src/services/api.ts
+++ frontend/src/services/api.ts
@@ -1,7 +1,8 @@
 import axios from "axios";
+import { BACKEND_BASE_URL } from "../config/config";
 
 const instance = axios.create({
-  baseURL: process.env.VUE_APP_BACKEND_BASE_URL,
+  baseURL: BACKEND_BASE_URL,
 });
 
 export const getMessage = async (): Promise<string> => {
